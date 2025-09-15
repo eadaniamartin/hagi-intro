5ce07286565536d15d1e6f206fba21940d31eb6f
MThomsen10 <math16@student.eadania.dk>
2025-09-15 14:46:37 +0200

    Udvikling af menu
    Mangler stadig at kunne returnere til menuen

--- HAGI-Opgave/HAGI-Opgave/Program.cs
+++ HAGI-Opgave/HAGI-Opgave/Program.cs
@@ -14,35 +14,64 @@ namespace HAGI_Opgave
             Console.WriteLine("=== MENU ===");
             Console.WriteLine("1. Marcus' Spil");
             Console.WriteLine("2. Martins Spil");
-            
+            bool rigtigtSvar = false; 
             // Her kommer et while loop som vurderer om den indtastede værdi er korrekt eller falsk
-            while (true)
+            while (rigtigtSvar == false)
             {
                 // VI GEMMER BRUGERENS RESPONS FRA KONSOLEN I EN STRING
                 string userResponse = Console.ReadLine();
                 // isNumber er en bool, som fortæller om TryParse af stringen userResponse kan laves til en integer, derudover får vi også en integer kaldet userResponseInteger
                 bool isNumber = int.TryParse(userResponse, out int userResponseInteger);
-                // her er en if statement som tjekker om den indskrevne værdi er et tal og om det er 1 eller 2
-                if (isNumber && userResponseInteger == 1 | userResponseInteger == 2)
+                // her er en if statement som tjekker om den indskrevne værdi er et tal
+                if (isNumber == true)
                 {
-                    Console.WriteLine($"Number: {userResponseInteger}");
-                    break;
+                    switch (userResponseInteger)
+                    {
+                        case 1:
+                            Console.Clear();
+                            MarcusGame();
+                            rigtigtSvar = true;
+                            break;
+                        case 2:
+                            Console.Clear();
+                            MartinsGame();
+                            rigtigtSvar = true;
+                            break;
+                        default:
+                            Console.WriteLine("Tallet er ikke 1 eller 2");
+                            break;
+                    }
                 }
-                // 
+                // Her er en else statement som køres hvis det ikke er et tal
                 else {
                     Console.WriteLine("Beklager, dit input er ikke et tal");
                 
                 }
             }
 
+        }
+ 
+        static void MarcusGame()
+        {
+            Console.WriteLine("Marcus spil");
 
-            Console.WriteLine("Perfekt du er kommet ud af loopet");
 
 
-            //for (int i = 0; i < 10; i++) {
-            //    Console.WriteLine(i);
-            //}
 
+        }
+
+        static void MartinsGame()
+        {
+            Console.WriteLine("Martins spil");
+
+            while(true)
+            {
+                string response = Console.ReadLine();
+                if (true)
+                {
+                    break;
+                }
+            }
 
         }
     }
